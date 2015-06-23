59d23238776670eded86643d425144a0724fb0a8
Alexander Nikitin <nikitindiz@gmail.com>
2015-06-23 19:51:49 +0600

    Добавлены файлы gulp

---
+++ gulp/tasks/production.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+
+// Run this to compress all the things!
+gulp.task('production', ['karma'], function(){
+  // This runs only if the karma tests pass
+  gulp.start(['markup', 'images', 'minifyCss', 'uglifyJs'])
+});
+
