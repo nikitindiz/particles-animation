6808d5aebd6f5e50efdacb702c646c20622c162f
Alexander Nikitin <nikitindiz@gmail.com>
2015-06-25 22:37:11 +0700

    Fixed #1 Квадраты двигаются туда-сюда

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,81 @@
-// // Browserify entry point for the page.js bundle (yay JavaScript!)
+var myDiv = document.querySelector('#place'),
+    width = window.innerWidth,
+    timerChecker;
 
-// var $ = require('jquery');
-// var _ = require('underscore');
-// // global.js already contains jQuery, so in our config.js file, we
-// // are exposing it to other files like this one in the `require` array.
-// // Also in config.js, jquery is listed in `external` array for this bundle.
-// // This combination lets this file use the jquery module bundled with
-// // global.js, instead of including it twice!
+window.resetPosition = function() {
+    myDiv.style.left = '0px';
+};
 
-// var messageTemplate = _.template("<p class='love-letter'>Made with <%= feels %> at <a href='<%= url %>'><%= bestCompanyEvar %>!</a></p>");
 
-// var message = messageTemplate({
-//   bestCompanyEvar: 'Viget',
-//   feels: '♥',
-//   url: 'http://viget.com'
-// });
+window.noTimer = function noTimer() {
 
-// $('body').append(message);
+    var myDiv = document.querySelector('#firstSquare');
+    
+    var position = parseInt(myDiv.style.left) || 0;
+    
+    while(position < (width / 2)) {        
+        position += 20;
+        myDiv.style.left = position + 'px';
+        position = parseInt(myDiv.style.left);
+    }
+    
+};
 
-console.log('page.js loaded!');
+window.firstMoveRight = true;
+window.seconMoveRight = true;
+
+window.withSetTimeout = function withSetTimeout() {
+
+    var myDiv = document.querySelector('#firstSquare');
+
+    var position = +myDiv.style.transform.replace(/.*\((\d+).*/, "$1") || 30;
+    
+    if ( position >= (width / 2) && window.firstMoveRight ) {
+        window.firstMoveRight = false;
+    } else if (position <= 20) {
+        window.firstMoveRight = true;
+    }
+
+    if (window.firstMoveRight) {
+        position += 1;
+        myDiv.style.transform = 'translateX(' + position + 'px)';
+    } else {
+        position -= 1;
+        myDiv.style.transform = 'translateX(' + position + 'px)';
+    }
+    
+    setTimeout(withSetTimeout, 0);
+
+};
+
+window.withRAF = function withRAF(time) {
+
+    var myDiv = document.querySelector('#secondSquare');
+
+    var position = +myDiv.style.transform.replace(/.*\((\d+).*/, "$1") || 30;
+    
+    if ( position >= (width / 2) && window.secondMoveRight ) {
+        window.secondMoveRight = false;
+    } else if (position <= 20) {
+        window.secondMoveRight = true;
+    }
+   
+    requestAnimationFrame(withRAF);
+
+    withRAF.time = withRAF.time || time;
+
+    var delta = (time - withRAF.time) || 1;
+    
+    if (window.secondMoveRight) {
+        position += delta;
+        myDiv.style.transform = 'translateX(' + position + 'px)';
+    } else {
+        position -= delta;
+        myDiv.style.transform = 'translateX(' + position + 'px)';
+    }
+
+    withRAF.time = time;
+
+    
+    
+};
